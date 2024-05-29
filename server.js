16243f814e356d60a31e8b76040dd62e50ee2ffc
22027998-Richard <134663996+22027998-Richard@users.noreply.github.com>
2024-05-29 14:46:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+//const express = require('express');
+//const app = express();
+//const port = 3000;
 
 // Serve the test.html file
-app.use(express.static('src'));
+//app.use(express.static('src'));
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+//app.listen(port, () => {
+  //console.log(`Server running at http://localhost:${port}/`);
+//});
